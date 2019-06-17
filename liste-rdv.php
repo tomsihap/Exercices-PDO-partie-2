0cfa7199a6b1e22c9393ad6572cdf1b9b6665b28
tomsihap <sihapanya.thomas@gmail.com>
2019-06-17 15:36:26 +0200

    Create liste-rdv.php

---
+++ liste-rdv.php
@@ -0,0 +1,64 @@
+<?php
+
+$bdd = new PDO('mysql:host=localhost;dbname=hospitale2n;charset=utf8;port=3306', 'root', '');
+$request = "SELECT *
+            FROM appointments
+            INNER JOIN patients
+                ON patients.id = appointments.idPatients";
+$response = $bdd->query($request);
+
+$appointments = $response->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row mt-3">
+            <div class="col-12">
+
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Date et heure</th>
+                            <th>Patient</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($appointments as $appointment) : ?>
+                            <tr>
+                                <td><?= $appointment['dateHour'] ?></td>
+                                <td>
+                                    <a href="profil-patient.php?id=<?= $appointment['idPatients'] ?>"><?= $appointment['firstname'] ?> <?= $appointment['lastname'] ?></a>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+
+                <a href="ajout-rdv.php" class="btn btn-primary float-right">Ajouter un RDV</a>
+
+            </div>
+        </div>
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
